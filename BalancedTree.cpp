e9918350f35e41e6e9f6f6fb73a217d4ed2834e7
siddhant <ss2033427@gmail.com>
2023-05-21 23:52:45 +0530

    Balanced tree

---
+++ BalancedTree.cpp
@@ -0,0 +1,180 @@
+#include<iostream>
+#include<pair>
+
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d) {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+node* buildTree(node* root) {
+
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);    
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+
+}
+
+void levelOrderTraversal(node* root) {
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL) { 
+            //purana level complete traverse ho chuka hai
+            cout << endl;
+            if(!q.empty()) { 
+                //queue still has some child ndoes
+                q.push(NULL);
+            }  
+        }
+        else{
+            cout << temp -> data << " ";
+            if(temp ->left) {
+                q.push(temp ->left);
+            }
+
+            if(temp ->right) {
+                q.push(temp ->right);
+            }
+        }
+    }
+
+}
+
+
+void buildFromLevelOrder(node* &root) {
+    queue<node*> q;
+
+    cout << "Enter data for root" << endl;
+    int data ;
+    cin >> data;
+    root = new node(data);
+    
+    q.push(root);
+
+    while(!q.empty()) {
+        node* temp = q.front();
+        q.pop();
+
+        cout << "Enter left node for: " << temp->data << endl;
+        int leftData;
+        cin >> leftData;
+
+        if(leftData != -1) {
+            temp -> left = new node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter right node for: " << temp->data << endl;
+        int rightData;
+        cin >> rightData;
+
+        if(rightData != -1) {
+            temp -> right = new node(rightData);
+            q.push(temp->right);
+        }
+    }
+ }
+int height(node *root){
+    if(root == NULL){
+        return 0;
+    }
+    return 1+max(height(root->left),height(root->right));
+}
+ bool isBalanced(node * root){
+    if(root == NULL){
+        return true;
+    }
+
+    bool left = isBalanced(root->left);
+    bool right = isBalanced(root->right);
+
+    bool diff = (abs(height(root->left) - height(root->right)) <=1);
+
+    if(left && right && diff){
+        return true;
+    }
+    else{
+        return false;
+    }
+ }
+pair<bool,int> isBalancedFast(node * root){
+    if(root == NULL){
+        pair<bool,int> p = make_pair(true,0);
+        return p;
+    }
+
+    pair<bool,int> left = isBalancedFast(root->left);
+    pair<bool,int> right = isBalancedFast(root->right);
+
+    bool leftAns = left.first;
+    bool rightAns = right.first;
+    bool diff = abs(left.second - right.second) <=1;
+
+    pair<bool,int> ans;
+
+    ans.second = max(left.second,right.second) +1;
+    if(leftAns&& rightAns && diff ){
+        ans.first = true;
+    }
+    else{
+        ans.first = false;
+
+    }
+    return ans;
+
+    
+}
+int main() {
+
+    node* root = NULL;
+
+    buildFromLevelOrder(root);
+    levelOrderTraversal(root);
+    // 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1
+    /*
+    //creating a Tree
+    root = buildTree(root);
+    //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1 
+    //level order
+    cout << "Printing the level order tracersal output " << endl;
+    levelOrderTraversal(root);
+
+    cout << "inorder traversal is:  ";
+    inorder(root); 
+
+    cout << endl << "preorder traversal is:  ";
+    preorder(root); 
+
+    cout << endl << "postorder traversal is:  ";
+    postorder(root); 
+    */
+
+
+    return 0;
+}
